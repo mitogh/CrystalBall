9557c40a246c52e615be755c1353efe03baa7127
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-02-27 16:19:20 -0600

    Little refactor on getString

--- src/com/example/crystalball/CrystallBall.java
+++ src/com/example/crystalball/CrystallBall.java
@@ -5,16 +5,14 @@ import java.util.Random;
 import android.content.Context;
 
 public class CrystallBall {
-	private String[] answers;
-	private Context context;
-	private Random random = new Random();
+	private String[] mAnswers;
+	private Random mRandom = new Random();
 	
 	public CrystallBall(Context context){
-		this.context = context;
-		answers = context.getResources().getStringArray(R.array.answers);
+		mAnswers = context.getResources().getStringArray(R.array.answers);
 	}
 	
 	public String getAnAnswer(){
-		return answers[random.nextInt(answers.length)];
+		return mAnswers[mRandom.nextInt(mAnswers.length)];
 	}
 }
